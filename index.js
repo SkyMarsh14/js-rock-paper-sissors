f376f24d8a99a836e89023c52db54d775780da8a
Sky Randy <soras1758@gmail.com>
2024-06-19 21:55:51 +0900

    Add winner's announcement and reset function if human or computer won 5 rounds.

--- index.js
+++ index.js
@@ -31,10 +31,13 @@ function playRound(humanChoice, computerChoice){
         return humanScore++;
     }else if(humanChoice===computerChoice){
         resultMessage.textContent="It's tie! ";
+        document.body.appendChild(resultMessage);
         //when it's not tie or win, then the only case left is "computer Win"
     }else{
         resultMessage.textContent=`You lose! ${computerChoice} beats ${humanChoice}`;
+        document.body.appendChild(resultMessage);
         return computerScore++;
+
     }
 }
 
@@ -54,9 +57,27 @@ const scissorsBtn=document.querySelector('#scissorsButton');
 
 const resultMessage=document.createElement('div');
 
+const scores=document.createElement('div');
 
 const btnsContainer=document.querySelector('.buttonContainer');
 btnsContainer.addEventListener('click',(event)=>{
+    scores.textContent='';
     let humanChoice=(event.target.textContent).toLowerCase();
     playRound(humanChoice,getComputerChoice());
+    scores.textContent=`Your Score:${humanScore} Computer Score:${computerScore}`;
+    document.body.appendChild(scores);
+
+    winnerAnnounce();
 })
+
+function winnerAnnounce(){
+    if(humanScore===5){
+        scores.textContent='You won 5 rounds! Congratulations!'
+        humanScore=0;
+        computerScore=0;
+    }else if(computerScore===5){
+        scores.textContent='Computer got 5 rounds! You lost by computer! Keep it up!'
+        computerScore=0;
+        humanScore=0;
+    }
+}
